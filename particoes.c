c04bf06c76212e02305d64920c3d62bd2106608b
Matheus Fernandes <matheusfernandesgomes1@gmail.com>
2021-08-23 19:14:57 -0300

    criação de partiçoes não ordenadas

--- particoes.c
+++ particoes.c
@@ -5,6 +5,7 @@
 
 #include "particoes.h"
 
+
 void classificacao_interna(FILE *arq, Lista *nome_arquivos_saida, int M, int nFunc) {
     rewind(arq); //posiciona cursor no inicio do arquivo
 
@@ -62,181 +63,111 @@ void classificacao_interna(FILE *arq, Lista *nome_arquivos_saida, int M, int nFu
 }
 
 void selecao_natural(FILE *arq, Lista *nome_arquivos_saida, int M, int nFunc, int n){
-    /*PARTE 1 TRABALHO LENDO M REGISTROS */
-    rewind(arq); //posiciona cursor no inicio do arquivo
 
-    int reg = 0;
-    int menor = INT_MAX;
-    int pos_menor=0,pos_inicial=0;
+    int reg = 0,pos_menor, menor=9999,k=0;
+    //pos_menor=menor_pos(*arq, M, nFunc);
+    //posicionando o cursor
+    rewind(arq);
+    //lendo o arquivo
+    TFunc *gv[nFunc];
 
     while (reg != nFunc) {
         //le o arquivo e coloca no vetor
-        TFunc *v[M],*menores[M];
-
+        TFunc *v[M];
         int i = 0;
         while (!feof(arq)) {
             fseek(arq, (reg) * tamanho_registro(), SEEK_SET);
             v[i] = le_funcionario(arq);
-       //     imprime_funcionario(v[i]);
+            gv[k]=v[i];
+            //imprime_funcionario(v[i]);
             i++;
+            k++;
             reg++;
             if(i>=M) break;
-        }
 
+            //*********************************************************************************************
+        }
         //ajusta tamanho M caso arquivo de entrada tenha terminado antes do vetor
         if (i != M) {
             M = i;
         }
-        /*//encontra o funcionario com menor chave no vetor
-        for(int s = 0; s < M; s++){
-            if(v[s]->cod < menor){
-                menor = v[s]->cod;
-                pos_menor = s;
-            }
-        }
-        */
-
-        //cria arquivo de particao e faz gravacao
-
-        char *nome_particao = nome_arquivos_saida->nome;
-        nome_arquivos_saida = nome_arquivos_saida->prox;
-        printf("\n%s\n", nome_particao);
-        FILE *m;
-        if ((m = fopen(nome_particao, "wb+")) == NULL) {
-            printf("Erro criar arquivo de saida\n");
-        } else {
-            for (int i = 0; i < M; i++) {
-                fseek(m, (i) * tamanho_registro(), SEEK_SET);
-                if(v[i]->cod < menor){
-                    menor = v[i]->cod;
-                    menores[i]->cod=menor;
-                    pos_menor = i;
-                    printf("1");//arquivo
-
-                }
-                else{
-                    for(pos_inicial; pos_inicial<pos_menor; pos_inicial++){
-                        salva_funcionario(menores[pos_inicial], m);
-                        printf("2"); //apagar
-                    }
-                    pos_inicial=pos_menor;
-
-                }
-            }  
-            fclose(m);
-            printf("3");//apagar
-        }
-        for(int jj = 0; jj<M; jj++){
-            imprime_funcionario(v[jj]);
-            free(v[jj]);
-            free(menores[jj]);
-        }
 
     }
-}
 
-     /*  char *nome_particao = nome_arquivos_saida->nome;
+      //Menor posição
+    for(int s = 0; s < k; s++){
+            if(gv[s]->cod < menor){
+                menor = gv[s]->cod;
+                pos_menor = s;
+            }
+        printf("*");
+        imprime_funcionario(gv[s]);
+    }
+
+    printf("menor %d, pos %d", menor,pos_menor);
+
+
+
+
+
+    //first
+    char *nome_particao = nome_arquivos_saida->nome;
         nome_arquivos_saida = nome_arquivos_saida->prox;
         printf("\n%s\n", nome_particao);
         FILE *p;
-        if ((p = fopen(nome_particao, "wb+")) == NULL)
-        {
+        if ((p = fopen(nome_particao, "wb+")) == NULL) {
             printf("Erro criar arquivo de saida\n");
-        }
-        else
-        {
-            printf("2");
-            for (int i = 0; i < M; i++){
-                fseek(p, (i) * tamanho_registro(), SEEK_SET);
-                if(v[i]->cod < menor)
-                {
-                    menor = v[i]->cod;
-                    menores[i]->cod=menor;
-                    pos_menor = i;
-                    printf('1');
-                }
-                else
-                {
-                    for(pos_inicial; pos_inicial<=pos_menor; pos_inicial++)
-                    {
-                        salva_funcionario(v[pos_inicial], p);
-
-                        imprime_funcionario(v[i]);
-                        pos_inicial=pos_menor;
-                    }
-                }
+        } else {
+            fseek(p, (0) * tamanho_registro(), SEEK_SET);
+            salva_funcionario(gv[pos_menor], p);
+            imprime_funcionario(gv[pos_menor]);
             }
+
+
+    //particao(gv[pos_menor], pos_menor,nome_arquivos_saida);
+    int y=1;
+    int next=pos_menor+1;
+    gv[pos_menor]->cod=0;
+
+    //ordena outros valores
+    int j=pos_menor+1;
+
+    while(j!=(pos_menor)){
+        if(y<M){
+
+            if ((p = fopen(nome_particao, "wb+")) == NULL) {
+                printf("Erro criar arquivo de saida\n");
+            }
+            else {
+                fseek(p, (0) * tamanho_registro(), SEEK_SET);
+                salva_funcionario(gv[j], p);
+                imprime_funcionario(gv[j]);
+            }
+            y++;
+
+        }
+        else{
             fclose(p);
-        }
-        for(int jj = 0; jj<M; jj++)
-        {
-            free(v[jj]);
-        }
-
-
-     /*   //faz o insertion sort
-        for (int j = 2; j <= M; j++) {
-        //posiciona o arquivo no registro j
-        fseek(arq, (j-1) * tamanho_registro(), SEEK_SET);
-        TFunc *fj = le_funcionario(arq);
-        //printf("\n********* Funcionario atual: %d\n", fj->cod);
-        i = j - 1;
-        //posiciona o cursor no registro i
-        fseek(arq, (i-1) * tamanho_registro(), SEEK_SET);
-        do{
-            TFunc *fi = le_funcionario(arq);
-            if( (fi->cod < fj->cod)){
-                break;
-            }
-            //printf("fi = %d\n", fi->cod);
-
-            //posiciona o cursor no registro i+1
-            fseek(arq, i * tamanho_registro(), SEEK_SET);
-          //  printf("Salvando funcionario %d na posicao %d\n", fi->cod, i+1);
-            salva_funcionario(fi, arq);
-            i = i - 1;
-            //l� registro i
-            fseek(arq, (i-1) * tamanho_registro(), SEEK_SET);
-           // fi = le_funcionario(arq);
-          //  printf("fi = %d; i = %d\n", fi->cod, i);
-
-            free(fi);
-        }while ((i > 0) );
-
-        //posiciona cursor no registro i + 1
-        fseek(arq, (i) * tamanho_registro(), SEEK_SET);
-        //printf("*** Salvando funcionario %d na posicao %d\n", fj->cod, i+1);
-        //salva registro j na posi��o i
-        salva_funcionario(fj, arq);
+            char *nome_particao = nome_arquivos_saida->nome;
+            nome_arquivos_saida = nome_arquivos_saida->prox;
+            printf("\n%s\n", nome_particao);
+            FILE *p;
+            j--;
+            y=0;
         }
 
 
-       /* //faz ordenacao NATURAL
-        int indice,menor,k;
-        for (int j = 1; j < M; j++) {
-            menor=v[j];
-            indice=j;
-            for(k=j+1;k< M; k++){
-                if(v[k]<menor){
-                    menor=v[k];
-                    indice=j;
-                }
-            }
-        v[indice]=v[i];
-        v[i]=menor;
+
+        j++;
+        if(j>=nFunc){
+            j=0;
         }
+    }
 
-        for (int j = 1; j < M; j++) {
-            TFunc *f = v[j];
-            i = j - 1;
-            while ((i >= 0) && (v[i]->cod > f->cod)) {
-                v[i + 1] = v[i];
-                i = i - 1;
-            }
-            v[i + 1] = f;
-        }*/
+}
 
-        /*//cria arquivo de particao e faz gravacao
+void particao(TFunc *gv, int pos,Lista *nome_arquivos_saida){
+    //cria arquivo de particao e faz gravacao
         char *nome_particao = nome_arquivos_saida->nome;
         nome_arquivos_saida = nome_arquivos_saida->prox;
         printf("\n%s\n", nome_particao);
@@ -244,14 +175,12 @@ void selecao_natural(FILE *arq, Lista *nome_arquivos_saida, int M, int nFunc, in
         if ((p = fopen(nome_particao, "wb+")) == NULL) {
             printf("Erro criar arquivo de saida\n");
         } else {
-            for (int i = 0; i < M; i++) {
-                fseek(p, (i) * tamanho_registro(), SEEK_SET);
-                salva_funcionario(v[i], p);
-                imprime_funcionario(v[i]);
+            fseek(p, (0) * tamanho_registro(), SEEK_SET);
+            salva_funcionario(gv, p);
+            imprime_funcionario(gv);
             }
             fclose(p);
-        }
-        for(int jj = 0; jj<M; jj++)
-            free(v[jj]);
 
-    }*/
+}
+
+
